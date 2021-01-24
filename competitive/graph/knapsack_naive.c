145e169cde38c1d05b174e09ff5887d1f3bd5157
johnresearches <76896708+johnresearches@users.noreply.github.com>
2021-01-23 22:02:26 -0500

    Added new

---
+++ competitive/graph/knapsack_naive.c
@@ -0,0 +1,34 @@
+/*
+	Naive Knapsack
+	
+*/
+
+//#include <bits/stdc++.h>
+#include <stdio.h>
+//using namespace std;
+
+int max(int a, int b){
+	return (a>b) ? a : b;
+}
+
+int knapsack(int W, int wt[], int val[], int n){
+	//base case
+	if(n == 0 || W == 0){
+		return 0;
+	}
+	
+	if(wt[n] > W){
+		return knapsack(W, wt, val, n-1);
+	}else{
+		return max(val[n] + knapsack(W - wt[n], wt, val, n-1), knapsack(W, wt, val, n-1));
+	}
+}
+
+int main(){
+	int val[] = {120,160,180};
+	int wt[] = {50, 60, 70};
+	int W = 100;
+	int n = sizeof(val) / sizeof(val[0]);
+	printf("%d", knapsack(W, wt, val, n));
+	return 0;
+}
---
+++ competitive/hr/botprin.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+
+void displayPathtoPrincess(int n, char grid[101][101]){
+    // declare variables
+    //    printf("RIGHT");
+    //    printf("\n");
+    //    printf("UP");  
+    
+    int botrows=0, botcols=0, prinrows=0, princols =0;
+    
+    if(n < 100 && n % 2 != 0){
+        for(int i = 0; i<n; i++){
+            for(int j= 0; j<n; j++){
+               if(grid[i][j]=='m' || grid[i][j]=='M'){
+                   botrows = i;
+                   botcols = j;
+               }else if(grid[i][j]=='p' || grid[i][j]=='P'){
+                   prinrows=i;
+                   princols=j;
+               }
+            }   
+        }
+        
+        // LEfT and RIGHT
+        if(botrows > prinrows){
+            while (botrows != prinrows){
+                botrows--;
+                printf("LEFT\n");
+            }
+        }else if(botrows < prinrows){
+            while (botrows != prinrows){
+                botrows++;
+                printf("RIGHT\n");
+            }
+        }
+        // UP and DOWN
+        if(botcols > princols){
+            while (botcols != princols){
+                botcols--;
+                printf("UP\n");
+            }
+        }else if(botcols < princols){
+            while (botcols != princols){
+                botcols++;
+                printf("DOWN\n");
+            }
+        }       
+    } 
+}
+
+int main(void) {
+
+    int m;
+    scanf("%d", &m);
+    char grid[101][101]={};
+    char line[101];
+
+    for(int i=0; i<m; i++) {
+        scanf("%s", line);
+        strcpy(grid[i], line);
+    }
+    displayPathtoPrincess(m,grid);
+    return 0;
+}
